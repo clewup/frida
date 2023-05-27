03a86f53d5b9d5b300e5dd1249c16da69e52cc80
clew <lewis@clewup.co.uk>
2023-05-27 18:22:44 +0100

    feat(daisy): add daisy ui and theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,16 +3,33 @@ module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+      }
+    }
   },
-  plugins: [],
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: [
+      {
+        store: {
+          primary: '#1FB2A5',
+          secondary: '#037d50',
+          accent: '#1FB2A5',
+          neutral: '#CCCCCC',
+          'base-100': '#3e3e42',
+          info: '#3ABFF8',
+          success: '#198754',
+          warning: '#FBBD23',
+          error: '#d9534f'
+        }
+      }
+    ]
+  }
 }
