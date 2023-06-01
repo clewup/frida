f266c04bd53565c52ea534aebd8c2faaf6dc4a86
clew <lewis@clewup.co.uk>
2023-06-01 17:46:27 +0100

    feat(error): add page error boundaries

---
+++ src/components/GenericError/GenericError.tsx
@@ -0,0 +1,34 @@
+import { useRouter } from 'next/navigation'
+import React, { type FC } from 'react'
+
+interface GenericErrorProps {
+  error: Error
+  reset?: () => void
+}
+
+const GenericError: FC<GenericErrorProps> = ({ error, reset }) => {
+  const router = useRouter()
+
+  return (
+      <main>
+          <input type="checkbox" id="my-modal" className="modal-toggle" checked={true} />
+          <div className="modal">
+              <div className="modal-box">
+                  <h3 className="font-bold text-lg text-center">Uh oh! Something went wrong!</h3>
+                  {error && <p className="py-4">Error: {error.message}</p>}
+                  <div className="modal-action justify-center gap-10">
+                      <button className="btn btn-primary" onClick={() => { router.push('/') }}>
+                          Home
+                      </button>
+                      {reset && (
+                          <button className="btn btn-secondary" onClick={reset}>
+                              Try Again
+                          </button>
+                      )}
+                  </div>
+              </div>
+          </div>
+      </main>
+  )
+}
+export default GenericError
