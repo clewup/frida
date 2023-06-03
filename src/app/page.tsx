550208328d4818a7bf798abfed867822a132bde9
clew <lewis@clewup.co.uk>
2023-06-03 19:25:21 -0400

    feat(categories): add categories banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,18 @@ import Product from '@/components/Product/Product'
 import Testimonial from '@/components/Testimonial/Testimonial'
 import constants from '@/constants/constants'
 import { type TestimonialType } from '@/types/testimonialTypes'
-import { type Product as PrismaProduct } from '@prisma/client'
+import { type Category, type Product as PrismaProduct } from '@prisma/client'
+import Link from 'next/link'
 import React from 'react'
 import { Users as UsersIcon } from 'react-feather'
 
 async function getProducts (): Promise<PrismaProduct[]> {
   const productsResponse = await fetch(`${constants.APP_URL}/api/product`)
+  return await productsResponse.json()
+}
+
+async function getCategories (): Promise<Category[]> {
+  const productsResponse = await fetch(`${constants.APP_URL}/api/category`)
   return await productsResponse.json()
 }
 
@@ -35,6 +41,7 @@ const mockTestimonials: TestimonialType[] = [
 
 export default async function Home () {
   const products = await getProducts()
+  const categories = await getCategories()
 
   return (
     <PageWrapper className="min-h-screen-header flex flex-col gap-20">
@@ -54,6 +61,17 @@ export default async function Home () {
         </span>
         <div className="grid grid-cols-3 gap-20">
           {mockTestimonials.map((testimonial, index) => (<Testimonial key={index} testimonial={testimonial}/>))}
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-5">
+        <h1 className="text-9xl font-bold">CATEGORIES</h1>
+        <div className={`grid grid-cols-${categories.length} gap-5 h-60`}>
+          {categories.map((category, index) => (
+              <Link href={`/catalogue?category=${category.name}`} key={index} className="border-[1px] border-primary rounded-md p-10 flex flex-col items-center justify-center">
+                <h1 className="uppercase text-4xl">{category.name}</h1>
+              </Link>
+          ))}
         </div>
       </div>
     </PageWrapper>
