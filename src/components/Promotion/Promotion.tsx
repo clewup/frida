fc857e3be3f171f36965a55715757e190d1549c7
clew <lewis@clewup.co.uk>
2023-06-08 17:30:33 -0400

    feat(promotion): enhance styling

--- src/components/Promotion/Promotion.tsx
+++ src/components/Promotion/Promotion.tsx
@@ -5,7 +5,7 @@ const Promotion = () => {
   return (
         <div>
             <div className="flex items-center gap-5">
-                <span className="w-1/2 text-center bg-primary text-base-100 text-xl">
+                <span className="w-1/2 text-center bg-primary text-base-100 text-xl rounded-tl-md py-2">
                     <p>New here? Get 10% off everything</p>
                     <p>with code: NEWHERE</p>
                 </span>
@@ -14,7 +14,7 @@ const Promotion = () => {
                     <p className="text-2xl">FREE SHIPPING ON ORDERS OVER £30!</p>
                 </span>
             </div>
-            <div className="text-center py-20 bg-secondary text-base-100">
+            <div className="text-center py-20 bg-secondary text-base-100 rounded-bl-md rounded-br-md">
                 <h1 className="font-bold text-5xl">20% OFF SELECTED PRODUCTS</h1>
                 <p className="text-3xl">with code: PROMO20</p>
             </div>
