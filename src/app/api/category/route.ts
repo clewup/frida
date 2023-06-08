be60274200ca49726a8ee0336acc92d290db7a0d
clew <lewis@clewup.co.uk>
2023-06-08 19:17:06 -0400

    fix(category): fix db query

--- src/app/api/category/route.ts
+++ src/app/api/category/route.ts
@@ -2,7 +2,7 @@ import { type NextRequest, NextResponse as response } from 'next/server'
 import prisma from '@/lib/prisma'
 
 export async function GET (request: NextRequest) {
-  const categories = await prisma.category.findMany()
+  const categories = await prisma.category.findMany({})
 
   return response.json(categories)
 }
