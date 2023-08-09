944051287f8e10c437f0c03cdfb226670c377b45
clew <lewis@clewup.co.uk>
2023-08-09 16:53:18 +0100

    chore(framer): remove framer-motion

--- src/components/Testimonial/Testimonial.tsx
+++ src/components/Testimonial/Testimonial.tsx
@@ -1,9 +1,6 @@
-'use client'
-
 import { type TestimonialType } from '@/types/testimonialTypes'
 import Image from 'next/image'
 import React, { type FC } from 'react'
-import { motion as m } from 'framer-motion'
 
 interface TestimonialProps {
   testimonial: TestimonialType
@@ -11,7 +8,7 @@ interface TestimonialProps {
 
 const Testimonial: FC<TestimonialProps> = ({ testimonial: { image, name, rating, review } }) => {
   return (
-        <m.div variants={{ hidden: { opacity: 0 }, visible: { opacity: 1, transition: { duration: 1 } } }} initial="hidden" animate="visible" className="p-10 flex flex-col gap-5 bg-white rounded-md">
+        <div className="p-10 flex flex-col gap-5 bg-white rounded-md">
                 <h1 className="text-xl">{review}</h1>
                 <span className="flex gap-5 items-center">
                     <div className="w-20 aspect-square relative">
@@ -19,7 +16,7 @@ const Testimonial: FC<TestimonialProps> = ({ testimonial: { image, name, rating,
                     </div>
                     <p>- {name}</p>
                 </span>
-        </m.div>
+        </div>
   )
 }
 
