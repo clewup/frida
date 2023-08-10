db8c1de9b0b530fb40f8ff649da434f8e097eabe
clew <lewis@clewup.co.uk>
2023-08-10 22:21:25 +0100

    feat(product): redesign cta buttons

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,12 +1,12 @@
 'use client'
 
-import Button from '@/components/Button/Button'
+import { Tooltip } from 'react-tooltip'
 import { useCart } from '@/contexts/CartContext/CartContext'
 import { type ProductType } from '@/types/productTypes'
 import Image from 'next/image'
 import Link from 'next/link'
 import React, { type FC } from 'react'
-import { ShoppingCart as CartIcon } from 'react-feather'
+import { Heart, ShoppingCart as CartIcon } from 'react-feather'
 
 interface ProductCardProps {
   product: ProductType
@@ -16,42 +16,59 @@ interface ProductCardProps {
 const ProductCard: FC<ProductCardProps> = ({ product, showAddToCartButton = true }) => {
   const { addToCart, isLoading } = useCart()
 
-  const { image, name, price, stock, subcategory: { name: subcategory } } = product
+  const { id, image, name, price, stock } = product
 
   return (
-    <div className="bg-white rounded-md flex flex-col justify-between overflow-hidden items-center">
-          <Link href={`/product/${name}`} className="p-10 w-full z-0">
+    <div className="bg-white rounded-md flex flex-col justify-between overflow-hidden items-center relative p-10 w-full">
+          <Link href={`/product/${name}`} className="p-10 w-full z-0 relative">
               <div className="relative w-full aspect-square">
                   <Image src={image} alt={name} fill={true} className="rounded-md transition-zoom"/>
               </div>
           </Link>
-          <div className="flex flex-col gap-3 text-center z-20 px-5">
-              <h2 className="text-gray-400">{subcategory}</h2>
-              <h1 className="text-xl">{name}</h1>
-
-              <p className="text-xl text-gray-400">£{Number(price).toFixed(2)}</p>
-          </div>
 
         {showAddToCartButton &&
-            <div className="p-10 flex justify-between items-center">
+            <div className="flex justify-center gap-2 items-center">
                 {stock > 0
                   ? (
-                        <Button
+                        <button
+                            data-tooltip-id={`cart-tooltip-${id}`}
+                            className="shadow-md bg-white rounded-[50%] p-3 aspect-square flex items-center justify-center hover:bg-black hover:text-white"
                             onClick={() => {
                               void addToCart(product)
                             }}
-                            isLoading={isLoading}
-                        >
-                            <CartIcon />
-                            Add to cart
-                        </Button>
+                            disabled={isLoading}>
+                            <CartIcon size={20} />
+                        </button>
                     )
                   : (
                         <p>Out of Stock</p>
                     )}
+                <button
+                    data-tooltip-id={`like-tooltip-${id}`}
+                    className="shadow-md bg-white rounded-[50%] p-3 aspect-square flex items-center justify-center cursor-not-allowed hover:bg-black hover:text-white"
+                    >
+                    <Heart size={20}/>
+                </button>
+
+                {/* Button Tooltips */}
+                <Tooltip
+                    id={`cart-tooltip-${id}`}
+                    place="top"
+                    content="Add to cart"
+                />
+                <Tooltip
+                    id={`like-tooltip-${id}`}
+                    place="top"
+                    content="Save for later"
+                />
             </div>
         }
 
+          <div className="flex flex-col gap-3 text-center z-20 pt-10 px-5">
+              <h1 className="text-xl">{name}</h1>
+
+              <p className="text-xl text-gray-400">£{Number(price).toFixed(2)}</p>
+          </div>
     </div>
   )
 }
